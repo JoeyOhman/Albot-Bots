cb4c5a5fa4f6a9f285b0e26b108a99919c354c8c
joeyohman <joeyeyey1995@gmail.com>
2018-09-28 00:05:45 +0200

    Teeeesting lib

--- Java/SnakeBotRandom/src/Main.java
+++ Java/SnakeBotRandom/src/Main.java
@@ -1,6 +1,7 @@
 import albot.Constants;
 import albot.snake.*;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,12 +13,14 @@ public class Main {
     static Random rand = new Random();
 
     public static void main(String[] args) {
+        /*
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter port number: ");
         int port = scanner.nextInt();
         game = new SnakeGame("127.0.0.1", port);
-        //game = new SnakeGame();
+        */
+        game = new SnakeGame();
 
         game.playGame((Main::decideMove), true);
 
@@ -45,27 +48,51 @@ public class Main {
         //game.restartGame();
 
         // Since this gives a class containing both playerMoves and enemyMoves, we specify playerMoves
-        List<String> possibleMoves = game.getPossibleMoves(board).playerMoves;
+        List<String> possibleMoves = board.getPossibleEnemyMoves();
+        for(String s : possibleMoves)
+            System.out.print(s + " ");
+        System.out.println();
+        List<String> nonLosingMoves = new LinkedList<>();
+
+
+        for(String possMove : possibleMoves) {
+            SnakeBoard simBoard = board.simulatePlayerMove(possMove);
+            BoardState bs = simBoard.evaluateBoardState();
+            System.out.println(possMove + ": " + bs);
+            if(bs != BoardState.enemyWon)
+                nonLosingMoves.add(possMove);
+        }
+
+        int dist = Integer.MAX_VALUE;
+        String moveToMake = "";
+        for(String move : nonLosingMoves) {
+            SnakeBoard simBoard = board.simulatePlayerMove(move);
+            BoardState bs = simBoard.evaluateBoardState();
+
+            int temp = squaredDistance(simBoard.player.x, simBoard.player.y, simBoard.enemy.x, simBoard.enemy.y);
+            if(temp < dist) {
+                dist = temp;
+                moveToMake = move;
+            }
+
+        }
+        if(!moveToMake.equals(""))
+            return moveToMake;
+
+
 
         int randomIndex = rand.nextInt(possibleMoves.size());
-        String randomMove = possibleMoves.get(randomIndex);
+        String move = possibleMoves.get(randomIndex);
 
-        SnakeBoard simBoard = game.simulatePlayerMove(board, randomMove);
-        simBoard.printBoard("My simulated board:");
-        simBoard = game.simulateEnemyMove(board, randomMove);
-        simBoard.printBoard("Enemy move board:");
-        simBoard = game.simulateMoves(board, randomMove, randomMove);
-        simBoard.printBoard("Both moves simmed:");
-        /*
-        for(int i = 0; i < 1000000; i++) {
-            while (game.evaluateBoard(simBoard) == BoardState.ongoing)
-                simBoard = game.simulatePlayerMove(simBoard, randomMove);
-            simBoard = new SnakeBoard(board);
-        }
-        */
+        //SnakeBoard simBoard = board.simulateMoves(move, move);
+        //simBoard.printBoard("Simulated board");
 
-        System.out.println(game.evaluateBoard(simBoard));
+        return move;
+    }
 
-        return randomMove;
+    private static int squaredDistance(int x1, int y1, int x2, int y2) {
+        int dx = x2-x1;
+        int dy = y2-y1;
+        return dx*dx + dy*dy;
     }
 }
